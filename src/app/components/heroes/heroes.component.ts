afdff7bac6c0048aa101bf90eb0416b94176dd83
dbcabanes <dborrascabanes@hotmail.com>
2019-10-08 11:51:38 +0200

    Routing Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 // Aqui importamos el Servicio heroes
 import { HeroesService, Heroe } from '../../services/heroes.service';
 
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -13,12 +16,21 @@ export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
 
   // En este constructor estamos llamando al sevicio HEROES
-  constructor(private _heroesService: HeroesService) { }
+  constructor(
+    private _heroesService: HeroesService,
+
+    // Funcion de redireccion
+    private _router:Router) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
 
+  }
+
+  // Funcion de redireccion, con el import de Router
+  verHeroe(idx: number) {
+    this._router.navigate(['/heroe', idx]);
   }
 
 }
